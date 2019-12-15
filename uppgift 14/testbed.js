f0ae38718da49c33745e23b23368a00db8cfaf0e
Emchen2001 <58258562+Emchen2001@users.noreply.github.com>
2019-12-15 16:02:01 +0100

    Add files via upload

---
+++ uppgift 14/testbed.js
@@ -0,0 +1,19 @@
+function	changeText(){
+
+
+var rubrik = document.getElementById('tjo');
+
+rubrik.innerHTML ='Du Klickade';
+
+
+
+
+
+}
+
+function changeBg() {
+	
+
+var tjo =document.getElementById('tjo');
+tjo.classList.add('nybg')
+}
